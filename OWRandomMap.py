f106789b17507f99c259a34ac0cd09349d91a746
Flo Stone <mail@flostone.com>
2019-03-21 13:46:09 +0100

    Fixed repeating maps

--- OWRandomMap.py
+++ OWRandomMap.py
@@ -2,6 +2,39 @@ import random
 import msvcrt
 
 DONT_PLAY_MODES_TWICE = True
+DONT_PLAY_MAPS_TWICE = True
+
+# Maps and gamemodes
+MAPS = {
+    "Assault": [
+        "Hanamura",
+        "Horizon Lunar Colony",
+        "Paris",
+        "Temple of Anubis",
+        "Volskaya Industries"
+    ],
+    "Escort": [
+        "Dorado",
+        "Junkertown",
+        "Rialto",
+        "Route 66",
+        "Watchpoint: Gibraltar"
+    ],
+    "Hybrid": [
+        "Blizzard World",
+        "Eichenwalde",
+        "Hollywood",
+        "King's Row",
+        "Numbani"
+    ],
+    "Control": [
+        "Busan",
+        "Ilios",
+        "Lijiang Tower",
+        "Nepal",
+        "Oasis"
+    ]
+}
 
 
 # wait for character input
@@ -10,45 +43,21 @@ def wait():
 
 
 # get a random map
-def get_random_map(exclude = None):
+def get_random_map(excludemode=None, excludemaps=None):
     # Modes to be selected from
     modes = ["Assault", "Escort", "Hybrid", "Control"]
-
-    # Maps to the modes
-    maps = {
-        "Assault": [
-            "Hanamura",
-            "Horizon Lunar Colony",
-            "Paris",
-            "Temple of Anubis",
-            "Volskaya Industries"
-        ],
-        "Escort": [
-            "Dorado",
-            "Junkertown",
-            "Rialto",
-            "Route 66",
-            "Watchpoint: Gibraltar"
-        ],
-        "Hybrid": [
-            "Blizzard World",
-            "Eichenwalde",
-            "Hollywood",
-            "King's Row",
-            "Numbani"
-        ],
-        "Control": [
-            "Busan",
-            "Ilios",
-            "Lijiang Tower",
-            "Nepal",
-            "Oasis"
-        ]
-    }
+    maps = MAPS
 
     # remove exclusion if needed
-    if exclude is not None:
-        modes = modes.remove(exclude)
+    if excludemode in modes:
+        modes.remove(excludemode)
+
+    # remove already played maps
+    if excludemaps is not None:
+        for key, values in maps.items():
+            for m in excludemaps:
+                if m in values:
+                    values.remove(m)
 
     # select a gamemode and map
     gamemode = random.choice(modes)
@@ -62,15 +71,27 @@ def get_random_map(exclude = None):
 def mainloop():
 
     gamemode = None
+    playedmaps = []
 
     while True:
 
+        # check if modes can be played twice in a row
         if not DONT_PLAY_MODES_TWICE:
             gamemode = None
 
-        # get a random map
-        selectedmap, gamemode = get_random_map(gamemode)
+        # check if maps can be played twice
+        if not DONT_PLAY_MAPS_TWICE:
+            playedmaps = None
 
+        # get a random map
+        selectedmap, gamemode = get_random_map(gamemode, playedmaps)
+
+        # add map to playedmaps
+        playedmaps.append(selectedmap)
+
+        print(playedmaps)
+
+        # print current map
         print("Your next Map is " + selectedmap)
 
         # wait for character
